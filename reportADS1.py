4edfe4d8f89f389e980176dcf20d243fef109c2c
Dakipaul <117090744+Dakipaul@users.noreply.github.com>
2022-12-15 04:54:24 +0530

    Add files via upload

---
+++ reportADS1.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec 14 08:23:32 2022
+
+@author: dakis
+"""
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import scipy.stats as stats
+
+def read_file 
